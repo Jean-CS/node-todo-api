248706eb980e61514103bc2013aa667f7c8c5463
Jean Santos <jeansantosc@gmail.com>
2018-07-18 22:29:12 -0300

    fetch with query

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,8 +9,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server.');
   const db = client.db('TodoApp')
 
-  // fetch all todos
-  db.collection('Todos').find().toArray().then((docs) => {
+  // fetch all todos that have not been completed yet
+  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
