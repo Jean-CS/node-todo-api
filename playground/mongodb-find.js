19d63a763d75f92e1b178d7b6a980f998f1ee081
Jean Santos <jeansantosc@gmail.com>
2018-07-18 22:26:02 -0300

    fetching data from mongodb

---
+++ playground/mongodb-find.js
@@ -0,0 +1,19 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb'); // object destructuring
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+
+  console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp')
+
+  // fetch all todos
+  db.collection('Todos').find().toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+});
