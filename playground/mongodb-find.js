1eebaf8ed372f2bb0710a921a9a7f03506ab2bd5
Jean Santos <jeansantosc@gmail.com>
2018-07-18 22:53:21 -0300

    fetch with query _id

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,8 +9,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server.');
   const db = client.db('TodoApp')
 
-  // fetch all todos that have not been completed yet
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+  // fetch todo with _id
+  db.collection('Todos').find({_id: '57bb12335d123bo43414'}).toArray().then((docs) => {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
