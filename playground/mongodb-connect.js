a3f65c517f8b9288c1c0221de8c079e731eca9be
Jean Santos <jeansantosc@gmail.com>
2018-07-18 21:49:51 -0300

    insert document to mongodb database

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,24 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server.');
   }
 
   console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp')
 
-  db.close();
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+
+    // '.ops' attribute stores all the docs that were inserted
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  client.close();
 });
