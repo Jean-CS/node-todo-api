5c623f4f248f2117551d8c0eeec0874b5c9fd15a
Jean Santos <jeansantosc@gmail.com>
2018-07-18 22:21:41 -0300

    object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb'); // object destructuring
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
