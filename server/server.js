3824beabbf671817d0448bf1e15052f7cd8bf1a3
Jean Santos <jeansantosc@gmail.com>
2018-07-19 22:07:10 -0300

    add POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 let express = require('express');
-let bodyParser = require('body-parser');
+let bodyParser = require('body-parser'); // takes the JSON and converts to object
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
-let app = expres();
+let app = express();
 
+app.use(bodyParser.json()); // parses every json request into an object
 
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
