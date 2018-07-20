07bafdd2ebb01432231046b20d0a5650dd594274
Jean Santos <jeansantosc@gmail.com>
2018-07-19 21:25:07 -0300

    user model and insert

--- server/server.js
+++ server/server.js
@@ -20,6 +20,15 @@ let Todo = mongoose.model('Todo', {
   }
 });
 
+let User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
 let newTodo = new Todo({
   text: 'Make lunch'
 });
@@ -28,4 +37,14 @@ newTodo.save().then((doc) => {
   console.log('Saved todo', doc);
 }, (err) => {
   console.log('Unable to save todo.');
+})
+
+let newUser = new User({
+  email: 'mongoose@user.com'
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved user', doc);
+}, (err) => {
+  console.log('Unable to save user.');
 })
