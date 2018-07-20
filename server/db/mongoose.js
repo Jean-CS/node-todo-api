365bc097d1a1fa1e6a3414eee001bedf80bcd272
Jean Santos <jeansantosc@gmail.com>
2018-07-19 21:52:10 -0300

    decoupling model and db instance

---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+let mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // use the javascript built-in Promises
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {
+  mongoose
+};
