65f795873e6735d10b9fbfceee3a40b6c0724057
Jean Santos <jeansantosc@gmail.com>
2018-07-19 21:54:43 -0300

    installid _express_ and _body-parser_

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
+let app = expres();
+
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
