4275f61c26e9178ae2276b94e5082fe4c552a0de
Jean Santos <jeansantosc@gmail.com>
2018-07-25 23:10:20 -0300

    add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser'); // takes the JSON and converts to object
 const {ObjectID} = require('mongodb');
@@ -65,6 +66,35 @@ app.delete('/todos/:id', (req, res) => {
 
     return res.send({todo});
   }).catch((() => res.status(400).send()));
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  // get only the Todo fields that the user can edit 
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body // updates the object
+  }, {
+    new: true // returns the updated object from the db
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({todo});
+  }).catch((err) => res.status(400).send());
 });
 
 app.listen(port, () => {
