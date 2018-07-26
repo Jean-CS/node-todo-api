23048a2eb25cda12b30e564949fd48ca1b3aded4
Jean Santos <jeansantosc@gmail.com>
2018-07-25 21:21:18 -0300

    database env variable for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // use the javascript built-in Promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
