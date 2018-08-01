6021ac03ad1c2e731b98b2d6a26de99772f47f38
Jean Santos <jeansantosc@gmail.com>
2018-07-31 22:59:46 -0300

    set jwt secret/salt into env var

--- server/models/user.js
+++ server/models/user.js
@@ -47,7 +47,7 @@ UserSchema.methods.generateAuthToken = function () {
   let token = jwt.sign({
     _id: user._id.toHexString(),
     access
-  }, 'abc123').toString();
+  }, process.env.JWT_SECRET).toString();
 
   //user.tokens.push({access, token});
   // OR
@@ -78,7 +78,7 @@ UserSchema.statics.findByToken = function (token) {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch(err) {
     // return new Promise((resolve, reject) => {
     //   reject();
