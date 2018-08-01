58c991b6030bed42b34cb6d889c4fe0d39f32955
Jean Santos <jeansantosc@gmail.com>
2018-07-31 22:43:56 -0300

    managing env variables from a file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
 const env = process.env.NODE_ENV || "development"; // sets to production or test... if not any of these two, set it to "development"
 
-console.log('env *****', env);
+if (env === 'development' || env === 'test') {
+  const config = require('./config.json');
+  const envConfig = config[env]; // either 'development' or 'test'
 
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
